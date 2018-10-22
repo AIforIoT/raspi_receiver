4379ac076f373024aeae6853bf50d60de3d9a182
Jan Fontanet <fontanet.jan@gmail.com>
2018-10-22 10:25:39 +0200

    Update

--- prova.py
+++ prova.py
@@ -4,10 +4,11 @@ import xmlrpc.client
 
 buff = np.ndarray([320000])
 ide = str(3)
-delay = str(4769900564563)
+delay = str(432)
 pos = str(50)
+timestamp = str(1540196375000)
 
 
-to_send = FrameData(np.array2string(buff), ide, delay, 'None', pos)
+to_send = FrameData(np.array2string(buff), ide, delay, 'None', pos, timestamp)
 client = xmlrpc.client.ServerProxy("http://localhost:8082/api")
 client.hello(to_send)
